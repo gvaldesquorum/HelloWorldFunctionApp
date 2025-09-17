7dcb89ae8e925568aefd6244661c97493b77672c
gvaldesquorum <german.valdes@quorumsoftware.com>
2025-09-17 15:12:39 -0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-import azure.functions as func
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Hello World function processed a request.')
-    return func.HttpResponse("Hello World from Azure Function!", status_code=200)
